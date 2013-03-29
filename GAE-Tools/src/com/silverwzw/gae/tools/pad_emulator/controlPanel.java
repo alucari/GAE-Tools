dabab8c4f5d142128d8cd02418cc73f7f3a3e231
Silverwzw <webmaster@silverwzw.com>
2013-03-29 06:27:02 -0400

    calendar

--- GAE-Tools/src/com/silverwzw/gae/tools/pad_emulator/controlPanel.java
+++ GAE-Tools/src/com/silverwzw/gae/tools/pad_emulator/controlPanel.java
@@ -66,7 +66,8 @@ final public class controlPanel extends ActionHandler {
 		for (String egg : freqEggs) {
 			o.println("document.write('[" + egg + "' + show(" + egg + ") + ']&nbsp;" +((++counter % 8 == 0)?"<br />')":"')"));
 		}
-		o.println("</script></body></html>");
+		String calendar = "<iframe src='https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=500&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=8k2qgo6ku4h30bjo0u6454fgv0%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FNew_York' style=' border-width:0 ' width='800' height='500' frameborder='0' scrolling='no'></iframe>";
+		o.println("</script><br /><br />" + calendar + "</body></html>");
 	}
 	final private static String td(String labelContent) {
 		return "<td>" + labelContent + "</td>";  
